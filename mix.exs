713178af5077d080e33207f21c4e84c5bf3e4881
Jake Morrison <jake.morrison@therealreal.com>
2022-09-19 10:05:26 -0500

    Add mix_audit

--- mix.exs
+++ mix.exs
@@ -46,6 +46,7 @@ defmodule SpandexTesla.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:mix_audit, "~> 2.0", only: [:dev, :test], runtime: false},
       {:bypass, "~> 2.1", only: :test, runtime: false},
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
