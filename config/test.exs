ee2b845fe75367e03b2231598805bd7861d63707
Jake Morrison <jake.morrison@therealreal.com>
2022-11-09 14:42:56 -0600

    Configure JUnit formatter

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,14 @@
 import Config
 
+config :junit_formatter,
+  report_file: "mix-test.xml",
+  report_dir: "#{Mix.Project.build_path()}/junit-reports",
+  automatic_create_dir?: true,
+  print_report_file: true,
+  # prepend_project_name?: true,
+  include_filename?: true,
+  include_file_line?: true
+
 config :logger,
   level: :warning
 
